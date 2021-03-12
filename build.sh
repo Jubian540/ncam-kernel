97d034df66f2d3b55eac620e6a923022e0c695ad
Jubian Zhu <jubian022@gmail.com>
2021-03-12 07:30:49 +0000

    Add default config for first build.
    Signed-off-by: Jubian Zhu <jubian022@gmail.com>

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@ CORE=`cat /proc/cpuinfo |grep "processor"|wc -l`
 THREAD=$(($CORE*2))
 
 make clean
+if [ ! -f .config ];then
+	make hi3516ev200_full_defconfig
+fi
 make uImage ARCH=arm CROSS_COMPILE=arm-himix100-linux- -j$THREAD
 
 cp arch/arm/boot/uImage ./kernel.bin
